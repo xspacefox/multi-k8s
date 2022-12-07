3439a55121d42f8774c683154d4d9654ad3a8109
Louise Ly <ly.louise07@gmail.com>
2022-12-07 01:27:31 -0600

    Commit before pushing to new repo

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const OtherPage = () => {
   return (
     <div>
       Im some other page!
@@ -9,3 +9,5 @@ export default () => {
     </div>
   );
 };
+
+export default OtherPage;
